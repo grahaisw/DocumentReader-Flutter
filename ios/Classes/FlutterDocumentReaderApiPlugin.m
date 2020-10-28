699f39267b2b44b213fc04986a162ff0b356fb84
Jenkins <jenkins@regula.by>
2020-10-28 13:54:07 +0300

    0.0.30

--- ios/Classes/FlutterDocumentReaderApiPlugin.m
+++ ios/Classes/FlutterDocumentReaderApiPlugin.m
@@ -346,7 +346,7 @@ typedef void (^Callback)(NSString* response);
 }
 
 - (void) addPKDCertificates:(NSArray*)input :(Callback)successCallback :(Callback)errorCallback{
-    NSMutableArray<RGLPKDCertificate*>* certificates = [[RGLPKDCertificate init] alloc];
+    NSMutableArray *certificates = [[NSMutableArray alloc] init];
     for(NSDictionary* certificateJSON in input)
         [certificates addObject:[JSONConstructor RGLPKDCertificateFromJson:certificateJSON]];
     [RGLDocReader.shared addPKDCertificates:certificates];
@@ -363,7 +363,11 @@ typedef void (^Callback)(NSString* response);
 }
 
 - (void) stopScanner:(Callback)successCallback :(Callback)errorCallback{
-    [RGLDocReader.shared stopScanner:^(){[self result:@"" :successCallback];}];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [RGLDocReader.shared stopScanner:^(){
+            [self result:@"" :successCallback];
+        }];
+    });
 }
 
 - (void) startNewSession:(Callback)successCallback :(Callback)errorCallback{
